08d9980d77ab083921a39ae1caff6d3726d5b95d
Michelle Macaraeg <macaraegm@gmail.com>
2020-10-08 14:12:26 -0700

    Added solution to repeatedString()

--- cloud_jumping.py
+++ cloud_jumping.py
@@ -9,9 +9,9 @@ def jumpingOnClouds(c):
 
     counter = 0
     i = 0
-    
     while i < len(c) - 1:
-        if c[i + 2] == 0:
+
+        if i < len(c) - 2 and c[i + 2] == 0:
             i += 2
         else:
             i += 1
@@ -19,6 +19,7 @@ def jumpingOnClouds(c):
         counter += 1
     return counter
 
-print(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]))
-print(jumpingOnClouds([0, 0, 0, 0, 1, 0]))
+print(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0])) #4
+print(jumpingOnClouds([0, 0, 0, 0, 1, 0])) #3
+print(jumpingOnClouds([0, 0, 0, 1, 0, 0])) #3
 
