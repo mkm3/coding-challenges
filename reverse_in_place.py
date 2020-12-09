1885eda04125bdfa1c2e5685dbf96158a865e8e3
Michelle Macaraeg <macaraegm@gmail.com>
2020-12-09 11:54:34 -0800

    Commented recursive solution and added non-recursive solution

--- reverse_in_place.py
+++ reverse_in_place.py
@@ -1,18 +1,53 @@
-def rev_list_in_place(lst):
-  """Reverse list in place.
+# def rev_list_in_place(lst):
+#   """Reverse list in place. You cannot do this with 
+#   reversed(), .reverse(), or list slice assignment!
+#   """
 
-  You cannot do this with reversed(), .reverse(), or list slice
-  assignment!
+#   #helper function - using recursion
+#   def helper(left,right):
+#     #if index/left is less that index/right
+#     if left < right:
+#       #left,right value is now right,left
+#       lst[left], lst[right] = lst[right], lst[left]
+#       #calling helper to move left pos to the right one and right pos to the left one
+#       #iteration by recursion
+#       helper(left + 1, right - 1)
+#   #function calling passing starting index pos and ending pos
+#   helper(0, len(lst) - 1)
+
+#   return lst
+
+
+
+def rev_list_in_place(lst):
+  """Reverse list in place. You cannot do this with 
+  reversed(), .reverse(), or list slice assignment!
   """
 
-  #helper function - using recursion
-  def helper(left,right):
-    if left < right:
-      lst[left], lst[right] = lst[right], lst[left]
-      helper(left + 1, right - 1)
+  i = 0
 
-  helper(0, len(lst)-1)
-
+  while i < len(lst) / 2:
+    #current pos, last index of the list - current pos = last index of the list - current pos, current pos
+    lst[i], lst[len(lst) - 1 - i] = lst[len(lst) - 1 - i], lst[i]
+    i += 1
   return lst
 
+
+  # #helper function - using recursion
+  # def helper(left,right):
+  #   #if index/left is less that index/right
+  #   if left < right:
+  #     #left,right value is now right,left
+  #     lst[left], lst[right] = lst[right], lst[left]
+  #     #calling helper to move left pos to the right one and right pos to the left one
+  #     #iteration by recursion
+  #     helper(left + 1, right - 1)
+  # #function calling passing starting index pos and ending pos
+  # helper(0, len(lst) - 1)
+
+  # return lst
+
 print(rev_list_in_place([3, 2, 1]))
+
+#take in list - reverse in place
+
